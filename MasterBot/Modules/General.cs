83f653510cd13fa0ddca24b242d4c60607079d71
Dominik Kurasbediani <d.kurasbediani@stud.elka.pw.edu.pl>
2021-06-19 16:14:18 +0200

    Added commands

---
+++ MasterBot/Modules/General.cs
@@ -0,0 +1,67 @@
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace MasterBot.Modules
+{
+    public class General : ModuleBase
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await Context.Channel.SendMessageAsync("Pong!");
+        }
+
+        [Command("info")]
+        public async Task Info(SocketGuildUser user = null) // Displays information about the user
+        {
+
+            user ??= (SocketGuildUser)Context.User;
+            var builder = new EmbedBuilder()
+                .WithThumbnailUrl(user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl())
+                .WithDescription("In this message you can see information about yourself!")
+                .WithColor(new Color(new Random().Next(0, 255), new Random().Next(0, 255), new Random().Next(0, 255)))
+                .AddField("User ID", Context.User.Id, true)
+                .AddField("Discriminator", user.Discriminator, true)
+                .AddField("Created at", user.CreatedAt.ToString("dd/MM/yyyy"), true)
+                .AddField("Joined at", (user as SocketGuildUser).JoinedAt.Value.ToString("dd/MM/yyyy"), true)
+                .AddField("Roles", string.Join(", ", (user as SocketGuildUser).Roles.Select(x => x.Mention)))
+                .WithCurrentTimestamp();
+            
+            var embed = builder.Build();
+            await Context.Channel.SendMessageAsync(null, false, embed);
+        }
+
+        [Command("purge")]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        public async Task Purge(int amount) // DELETES amount MESSAGES IF PERMISISONS ARE RIGHT
+        {
+            var messages = await Context.Channel.GetMessagesAsync(amount + 1).FlattenAsync(); // amount + 1 also removes the message that called for purge
+            await (Context.Channel as SocketTextChannel).DeleteMessagesAsync(messages);
+
+            var message = await Context.Channel.SendMessageAsync($"{messages.Count()} messages deleted successfully!");
+            await Task.Delay(2500); // wait 2.5 seconds
+            await message.DeleteAsync();
+        }
+
+        [Command("server")]
+        public async Task Server() // Displays information about the server
+        {
+            var builder = new EmbedBuilder()
+                .WithThumbnailUrl(Context.Guild.IconUrl)
+                .WithDescription("Information about the server.")
+                .WithTitle($"{Context.Guild.Name} Information")
+                .WithColor(new Color(new Random().Next(0, 255), new Random().Next(0, 255), new Random().Next(0, 255)))
+                .AddField("Created at", Context.Guild.CreatedAt.ToString("dd/MM/yyyy"))
+                .AddField("Member count", (Context.Guild as SocketGuild).MemberCount + " members", true)
+                .AddField("Online Users", (Context.Guild as SocketGuild).Users.Where(x => x.Status != UserStatus.Offline).Count() + " members", true);
+
+            var embed = builder.Build();
+            await Context.Channel.SendMessageAsync(null, false, embed);
+        }
+    }
+}
--- MasterBot/Services/CommandHandler.cs
+++ MasterBot/Services/CommandHandler.cs
@@ -22,6 +22,30 @@ namespace MasterBot.Services
             _config = config;
 
             _discord.Ready += OnReady;
+            _discord.MessageReceived += OnMessageReceived;
+        }
+
+        private async Task OnMessageReceived(SocketMessage arg)
+        {
+            var msg = arg as SocketUserMessage;
+            if(msg.Author.IsBot) return; // does nothing if the author is bot
+
+            var context = new SocketCommandContext(_discord, msg);
+
+            int pos = 0;
+            if(msg.HasStringPrefix(_config["prefix"], ref pos) || msg.HasMentionPrefix(_discord.CurrentUser, ref pos)) // ensures the message contains the prefix or hte bot is tagged
+            {
+                var result = await _commands.ExecuteAsync(context, pos, _provider); // sends context, pos and provider to commands
+
+                if(!result.IsSuccess)
+                {
+                    var reason = result.Error;
+
+                    await context.Channel.SendMessageAsync($"The following error has occurred:\n{reason}"); // posts the error to the chat
+                    Console.WriteLine(reason); // logs error to console, need to expand it
+                }
+            }
+
         }
 
         private Task OnReady()
