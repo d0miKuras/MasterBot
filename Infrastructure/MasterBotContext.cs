c6b265778afae25e7229dce80c15e8dc7766b2a8
Dominik Kurasbediani <d.kurasbediani@stud.elka.pw.edu.pl>
2021-06-21 15:12:53 +0200

    Edited README, commiting old commits

---
+++ Infrastructure/MasterBotContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class MasterBotContext : DbContext
+    {
+        public DbSet<Server> Servers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) 
+            => options.UseMySql("server=localhost;user=root;database=masterbot;port=3306;", new MariaDbServerVersion(new Version(10, 4, 19))); // new Version(8, 0, 21) https://github.com/PomeloFoundation/Pomelo.EntityFrameworkCore.MySql/pull/1233
+    }
+    public class Server
+    {
+        public ulong Id { get; set; }
+        public string Prefix { get; set; }
+        
+    }
+}
